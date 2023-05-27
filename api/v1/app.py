7f4e1114445fed31b95fb48b395df8d885a4a49d
exuszee <exuszee@gmail.com>
2023-05-27 16:39:45 +0300

    3

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,8 +10,6 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -20,8 +18,7 @@ def teardown(exception):
     """
     storage.close()
 
-
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
