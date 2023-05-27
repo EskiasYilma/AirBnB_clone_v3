66fe2295bade177fab3e49fd571b88d7d29644f3
exuszee <exuszee@gmail.com>
2023-05-27 18:10:57 +0300

    merged with storage_get_count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,27 @@ API page
 """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
-def get_status():
+def status():
     """
     get api status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """
+    get stats from models
+    """
+    model_dict = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+    return jsonify({"status": "sss"})
