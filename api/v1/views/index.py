e5083ebe3de338b59d878da15425a057b1ef0684
exuszee <exuszee@gmail.com>
2023-05-27 16:40:58 +0300

    3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def get_status():
     """
     get api status
