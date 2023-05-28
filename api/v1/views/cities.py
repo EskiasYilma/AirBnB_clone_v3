f89bbe12749d63708a00ea5f36360c872511eadc
exuszee <exuszee@gmail.com>
2023-05-28 21:10:52 +0300

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -15,6 +15,7 @@ import os
 methods = ["GET", "POST", "DELETE", "PUT"]
 
 
+@app_views.route("/states/<state_id>/cities/", methods=[methods[0]])
 @app_views.route("/states/<state_id>/cities", methods=[methods[0]])
 def all_cities(state_id=None):
     """
