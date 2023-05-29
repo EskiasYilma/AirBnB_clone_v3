18c74c7fb53ffffae595e83ef1793294c4b982e6
exuszee <exuszee@gmail.com>
2023-05-29 16:59:20 +0300

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,14 @@ starts a Flask web application
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
